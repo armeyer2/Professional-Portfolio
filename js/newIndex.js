54296ed34c2e62615cb3b47a5e5ead1288dbac49
Aaron Meyer <armeyer2@madisoncollege.edu>
2018-04-10 21:49:09 -0500

    Fixed positioning on loading screen
    Still have hamburger problems and price display on mobile

--- js/newIndex.js
+++ js/newIndex.js
@@ -8,7 +8,7 @@
         $("#jumbotronImage").width($(window).width());
 
           $(".portfolioFadeText").fadeIn(1500);
-          $(".gifLoader").fadeOut(1500);
+          $("#gifLoader").fadeOut(1500);
 
           //var ctx = document.getElementById("myChart");
           /**var myChart = new Chart(ctx, {
