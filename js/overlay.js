2780285bdd9632bc7ff768c2f9278349706a0cbc
Aaron Meyer <31418505+armeyer2@users.noreply.github.com>
2018-04-25 15:15:42 -0500

    Added navbar and front text

--- js/overlay.js
+++ js/overlay.js
@@ -7,27 +7,6 @@
 
     });
 
-
-    $('#indexCapture').mouseover(function () {
-        $(this).css("border" , "#a3a09c 2px solid");
-    })
-    $('#indexCapture').mouseout(function () {
-        $(this).css("border" , "#e0dedb 2px solid");
-    })
-    $('#infoCapture').mouseover(function () {
-        $(this).css("border" , "#a3a09c 2px solid");
-    })
-    $('#infoCapture').mouseout(function () {
-        $(this).css("border" , "#e0dedb 2px solid");
-    })
-
-    $('#galleryCapture').mouseover(function () {
-        $(this).css("border" , "#a3a09c 2px solid");
-    })
-    $('#galleryCapture').mouseout(function () {
-        $(this).css("border" , "#e0dedb 2px solid");
-    })
-
     $('#portfolioTextContainer').mouseover(function () {
         $("#pageLoadOverlay").fadeOut(250);
 
@@ -36,7 +15,7 @@
         }, 1000)
     })
 
-    
+
 
 
 })(jQuery);
